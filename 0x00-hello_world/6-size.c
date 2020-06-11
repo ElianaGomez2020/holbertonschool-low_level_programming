a9c0fb01b9824d9191bdfc063d9af6865304cbf7
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-06-11 04:47:57 +0000

    Task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
-
 /**
-  * main - prints string
-  * Return: Always 0 (Sucess)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-{	char:
-
-        printf("with proper grammar, but the outcome is a piece of art,\n");
+{
+        char ch;
+        int in;
+        long int lin;
+        long long int llin;
+        float flot;
+        printf("Size of a char: %ld byte(s)\n", sizeof(ch));
+        printf("Size of an int: %ld byte(s)\n", sizeof(in));
+        printf("Size of a long int: %ld byte(s)\n", sizeof(lin));
+        printf("Size of a long long int: %ld byte(s)\n", sizeof(llin));
+        printf("Size of a float: %ld byte(s)\n", sizeof(flot));
         return (0);
 }
-
