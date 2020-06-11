ceb6b69b865334aeddb76dd80a5060b49704def7
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-06-11 03:47:39 +0000

    task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * main - prints string
+  * Return: Always 0 (Sucess)
+*/
+int main(void)
+{	char:
+
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
+
