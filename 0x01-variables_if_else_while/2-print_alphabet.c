3466ed74db93ba6c12c3abfa6ad77393d233c451
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-06-12 00:40:52 +0000

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	char A;
+
+	for (A = 'a'; A <= 'z' ; A++)
+	{
+		putchar(A);
+	}
+	putchar('\n');
+	return (0);
+}
