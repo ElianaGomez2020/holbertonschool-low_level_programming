18c4a243d058d92d8d2c5be385609619a9bb6f58
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-06-13 20:56:53 +0000

    task 10.6

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -9,16 +9,16 @@ int main(void)
 	int num1;
 	int num2;
 
-	for (num1 = '0' ; num1 <= '9' ; num1++)
+	for (num1 = 48 ; num1 <= 57 ; num1++)
 	{
-		for (num2 = '0' ; num2 <= '9' ; num2++)
+		for (num2 = 48 ; num2 <= 57 ; num2++)
 		{
 			putchar(num1);
 			putchar(num2);
-			if (num1 != '9' || num2 != '9')
+			if (num1 != 57 || num2 != 57)
 				putchar(',');
 				putchar(' ');
-		}
+		} 
 	}
 	putchar('\n');
 	return (0);
