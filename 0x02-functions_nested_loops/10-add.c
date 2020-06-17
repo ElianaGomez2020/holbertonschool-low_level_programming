da2b812900994ef4d1a4caf3c0d7349ddc3fd650
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-06-17 21:12:34 +0000

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ *add - is the function.
+ *@a: value for int
+ *@b: value for int
+ * Return: Always 0
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
