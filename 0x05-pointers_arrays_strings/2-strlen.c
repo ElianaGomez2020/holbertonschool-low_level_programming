4e5881ecb133cfbfbad5d18cdd13aa69a4a3d6fa
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-06-25 00:07:43 +0000

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ *_strlen -  function counts the number of characters in a given string
+ *@s: is the pointer
+ *Return: len
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	for (len = 0 ; s[len] != '\0' ; len++)
+	{
+	}
+	return (len);
+}
