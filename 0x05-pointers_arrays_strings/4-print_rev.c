2caef79140001331dee5666412a8ff3bfb93a287
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-06-25 15:11:00 +0000

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -19,3 +19,17 @@ void print_rev(char *s)
 	_putchar ('\n');
 }
 
+/**
+ *_strlen -  function counts the number of characters in a given string
+ *@s: is the pointer
+ *Return: len
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	for (len = 0 ; s[len] != '\0' ; len++)
+	{
+	}
+	return (len);
+}
