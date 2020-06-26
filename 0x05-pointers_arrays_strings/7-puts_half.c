793b6c1a68308ece9068dcf5644de7935614cb0e
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-06-26 23:18:24 +0000

    task 7.2

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,12 +13,9 @@ void puts_half(char *str)
 	for (a = 0 ; str[a] != '\0' ; a++)
 	{
 	}
-	if (a % 2 == 0)
+	for ((b = (a + 1) / 2); str[b] != '\0' ; b++)
 	{
-		for ((b = (a + 1) / 2); str[b] != '\0' ; b++)
-		{
-			_putchar(str[b]);
-		}
+		_putchar(str[b]);
 	}
 	_putchar('\n');
 }
