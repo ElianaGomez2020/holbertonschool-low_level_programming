4bb571a8f02c143f49bc8b5b876eef050c2c8520
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-06-26 04:27:36 +0000

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *puts2 -  function print every other character
+ *@str: is the pointer
+ *Return: 0
+ */
+void puts2(char *str)
+{
+	int a;
+
+	for (a = 0 ; str[a] > 10 ; a = a + 2)
+	{
+		_putchar (str[a]);
+	}
+	_putchar('\n');
+}
