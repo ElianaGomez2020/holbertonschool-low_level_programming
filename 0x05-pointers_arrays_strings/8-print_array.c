2178a815fdf5f13b55740fb8b21e3d216a2876f7
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-06-27 03:20:03 +0000

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *print_array - function prints n elements of an array
+ *@a: is the pointer
+ *@n: is an integer
+ *Return: 0
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0  ; i < n ; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
