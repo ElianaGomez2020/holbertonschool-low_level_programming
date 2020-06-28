5807b029bf66b96e437bbbdff2a60f5b18eb7a3b
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-06-28 20:01:10 +0000

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+/**
+ *_strcat - function appends the src string to the dest string
+ *@dest: string dest
+ *@src: strind appends
+ *Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a, b;
+
+	for (a = 0; dest[a] != '\0'; a++)
+	{
+	}
+	for (b = 0; src[b] != '\0'; b++)
+	{
+		dest[a + b] = src[b];
+	}
+	dest[a + b] = '\0';
+	return (dest);
+}
