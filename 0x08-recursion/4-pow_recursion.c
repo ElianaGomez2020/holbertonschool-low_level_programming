b51bb4d449855d5659f56da4cb1d5fa881bb213d
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-07-02 02:41:42 +0000

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+/**
+ * _pow_recursion - function that returns the value .
+ * @x: value of parameter
+ * @y: value of raised
+ *
+ * Return: the function
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y <= 0)
+	{
+		return (-1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
