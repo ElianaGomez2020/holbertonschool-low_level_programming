75e56ccbf86e8545d43e92afbd46f605880f4383
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-07-05 23:33:59 +0000

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - argc argv
+ * @argc: int
+ * @argv: char
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argv)
+	{
+		printf("%d\n", argc - 1);
+	}
+	return (0);
+}
