53582a9f8fe8bf6fa0f76bcaf4f631382e6af310
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-07-05 22:46:47 +0000

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - argc argv
+ * @argc: int
+ * @argv: char
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	if (argc)
+	{
+		printf(" %s\n", argv[0]);
+	}
+	return (0);
+}
