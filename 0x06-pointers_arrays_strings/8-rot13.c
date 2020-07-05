968a2ca1ea1c37a66a7bbdadaa360566a866c8c7
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-07-05 03:30:42 +0000

    task 8

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,22 @@
+/**
+ * rot13 - code a string to rot13.
+ * @s: string to convert
+ * Return: s
+ */
+char *rot13(char *s)
+{
+	int a, b;
+	char letter[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (b = 0; letter[b] != '\0'; b++)
+		{
+			if (s[a] == letter[b])
+				s[a] = rot13[b];
+				break;
+		}
+	}
+	return (s);
+}
