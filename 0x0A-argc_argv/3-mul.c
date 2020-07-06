cf3b8a3f9db6a15bd47f88d09c2f150ec8f6a63e
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-07-06 02:42:24 +0000

    task 3.1

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,7 +14,8 @@ int main(int argc, char *argv[])
 
 	if (argc == 3)
 	{
-		printf("%d \n", atoi(argv[1]) * atoi(argv[2]));
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (1);
 	}
 	else
 	{
