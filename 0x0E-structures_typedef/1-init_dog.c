e13fb039420f2b3bdf667b3bb14d8fe7fbcacfea
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-07-15 02:13:44 +0000

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - structure
+ * @d: dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (!d)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	
+}
