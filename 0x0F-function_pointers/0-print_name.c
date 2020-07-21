044e4ac66a19b5fa6bb3ecdfc0130f90ae03afc6
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-07-21 22:22:04 +0000

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function pointer to a function
+ * @name: pointer
+ * @f: pinter function
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
