08d443c7277f0822778e6c61e7fef73487115008
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-07-29 17:31:35 +0000

    Task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ *print_list - prints elements of list
+ *@h: const
+ *Return: num of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	for (i = 0; h != '\0'; i++)
+	{
+		if (h->str)
+			printf("[%u] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+		h = h->next;
+	}
+	return (i);
+	}
