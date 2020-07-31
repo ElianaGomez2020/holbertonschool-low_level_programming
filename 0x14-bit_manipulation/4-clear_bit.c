511f8dae813d8925efa4494aaa1260df9090fac3
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-07-31 01:00:06 +0000

    task

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *clear_bit - sets the value of a bit to 1 at a given index.
+ *@n: number convert
+ *@index: is the index
+ *Return: the value of the bit
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+	*n = *n & ~(1 << index);
+	return (1);
+}
