aa5ca6e2352827256d5327998811e9c98189ffb8
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-08-05 01:42:58 +0000

    Task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,9 @@
 #include "holberton.h"
 /**
- * 
- * 
+ * read_textfile - reads a text file and prints it to the  standard output.
+ * @filename: file descriptor
+ * @letters: numb of letters it should read and print
+ * Return: number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -9,27 +11,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd;
 	char *buf;
 
-	if(!filename)
-		return(0);
-
-	buf =malloc(sizeof(char) * letters);
-	if(buf == NULL)
-		return(0);
-
+	if (!filename)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
 	fd = open(filename, O_RDWR);
 	if (fd == -1)
 		return (0);
 	n = read(fd, buf, letters);
-	if(n == -1)
-		return(0);
-	n= write(STDOUT_FILENO, buf, n);
+	if (n == -1)
+		return (0);
+	n = write(STDOUT_FILENO, buf, n);
 	free(buf);
 	close(fd);
-		
-	
-	
-	
-	return(n);
-	
-
+	return (n);
 }
