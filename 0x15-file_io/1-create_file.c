decafa06d38f8ddefdab672863e6ebbe1a37ff0f
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-08-05 02:44:08 +0000

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 /**
- * create_file - function will create a file
- * @filename:  name of file to be created
- * @text_content: text to be written in new file
- * Return: function will return 1 on success, -1 on fail
+ * create_file - function that creates a file.
+ * @filename:  name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on success, -1 on fail
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i = 0, n_write;
+	int fd, i = 0, n;
 
 	if (filename == NULL)
 		return (-1);
@@ -18,8 +18,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	while (text_content[i] != '\0')
 		i++;
-	n_write = write(fd, text_content, i);
-	if (n_write == -1)
+	n = write(fd, text_content, i);
+	if (n == -1)
 		return (-1);
 	return (1);
 }
