111bc2181e24589592bada269f995d1a1895580e
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-08-05 02:52:17 +0000

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,7 +7,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i = 0, n;
+	int fd, n, i;
 
 	if (filename == NULL)
 		return (-1);
@@ -16,8 +16,9 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	while (text_content[i] != '\0')
-		i++;
+	for (i = 0; text_content[i] != '\0'; i++)
+	{
+	}
 	n = write(fd, text_content, i);
 	if (n == -1)
 		return (-1);
