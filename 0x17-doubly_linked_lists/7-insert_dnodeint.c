5b7d31344c232cb08fed5028a1b8fcfae242d007
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-09-05 20:25:34 +0000

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,6 +9,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *tmp = *h, *node;
+
 	if (h == NULL || *h == NULL)
 		return (NULL);
 	node = malloc(sizeof(dlistint_t));
@@ -27,6 +28,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		idx--;
 		if (tmp->next == NULL && (idx - 1) == 0)
 			return (add_dnodeint_end(h, n));
+		if (tmp->next == NULL && (idx - 1) > 0)
+			return (NULL);
 	}
 	node->n = n;
 	tmp->prev->next = node;
