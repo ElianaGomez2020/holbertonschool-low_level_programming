1c2cdd2d0710d5251acc97df46a90477605801f4
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-09-05 20:14:02 +0000

    task7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -24,8 +24,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		tmp = tmp->next;
 		idx--;
-		if (tmp->next == NULL && idx - 1 == 0)
-			return (add_dnodeint_end(h, n));;
+		if (tmp->next == NULL && (idx - 1) == 0)
+			return (add_dnodeint_end(h, n));
+		if (tmp->next == NULL && (idx - 1) != 0)
+			return (NULL);
 	}
 	node->n = n;
 	tmp->prev->next = node;
