4b8c347d42cdbe0859214098f1d2f38f0be5692d
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-09-05 21:47:25 +0000

    task

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,7 +9,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *aux = *head;
 
-	if (!head || *head == NULL)
+	if (head  == NULL || *head == NULL)
 		return (-1);
 	if (index == 0)
 	{
