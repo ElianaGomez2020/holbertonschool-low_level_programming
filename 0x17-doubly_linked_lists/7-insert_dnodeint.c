d01df93dc906ae02d30a9ef7b60270153ed99532
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-09-05 21:22:17 +0000

    task

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,7 +16,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (tmp->next == NULL && idx == 1)
 		return (add_dnodeint_end(h, n));
-
+	if (tmp->next == NULL && idx > 1)
+		return (NULL);
 	while (idx != 0)
 	{
 		tmp = tmp->next;
