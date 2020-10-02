fc8ce50b518e3c7206209b313af5d77802de413e
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-10-02 16:40:59 +0000

    tasks

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+/**
+ *hash_table_create - create hash table
+ *@size: is the size of the array
+ * Return: pointer
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht = NULL;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
+	return (ht);
+}
